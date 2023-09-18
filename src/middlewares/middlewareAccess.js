2781efeb1dbdc037a88e3bcb1d46717372833a10
matiSolis <solis.matias.n@gmail.com>
2023-09-17 21:53:03 -0300

    comit a medias

--- src/middlewares/middlewareAccess.js
+++ src/middlewares/middlewareAccess.js
@@ -10,14 +10,14 @@ export const adminSession = (req, res, next) => {
   next();
 };
 
-export const privateAcces = (req, res, next) => {
-  if (!req.session.user) return res.redirect('/login');
-  next();
-};
-
 export const premiumSession = (req, res, next) => {
   if (req.session.user.role !== 'Premium') {
     return res.status(403).send({ error: { status: 403, message: 'Access denied.' } });
   };
+  next();
+};
+
+export const privateAcces = (req, res, next) => {
+  if (!req.session.user) return res.redirect('/login');
   next();
 };
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controllers.js';
+import { adminSession, premiumSession } from '../middlewares/middlewareAccess.js';
 
 const router = Router();
 const productController = new ProductController();
 
 router.get('/', productController.getProducts);
-router.post('/', productController.addProduct);
+router.post('/', adminSession, premiumSession, productController.addProduct);
 router.get('/:pid', productController.getProductById);
 router.put('/:pid', productController.updateProductById);
-router.delete('/:pid', productController.deleteProductById);
+router.delete('/:pid', adminSession, productController.deleteProductById);
 export default router;
